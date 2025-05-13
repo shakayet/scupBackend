680ba7c24dcd1d8d0a6edbcaf6c8e375305ec3ee
shakayet <srabonshakhawat@gmail.com>
2025-05-13 20:00:37 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+// MongoDB connection.
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch(err => {
+  console.error('MongoDB connection failed:', err.message);
+});
+
